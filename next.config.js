55cce946ed299d31d65b2c29996b16d47a5618b4
acervmon <126948783+acervmon@users.noreply.github.com>
2026-01-01 17:06:45 +0100

    Fix Next config and dependencies for Amplify SSR build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No usar output: "export" para evitar la necesidad de generateStaticParams()
-  // Esto permitirá que las páginas dinámicas funcionen con Client Components
+  // No usar output: "export" para que SSR funcione
 };
 
 module.exports = nextConfig;
